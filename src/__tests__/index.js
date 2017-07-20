b431d02d0e78673eabec99a200075d94f9051ce7
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-20 17:06:03 +0100

    refactored

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,12 +1,82 @@
-import {GlobalInputReceiver} from "../index"
-import React, {Component} from 'react'
-import {createMessageConnector} from "global-input-message";
+import {GlobalInputReceiver,GlobalInputSender} from "../index";
+import React, {Component} from 'react';
+
+import renderer from 'react-test-renderer';
+
+test("sender and receiver communication", function(done){
+      var receiver=null;
+      var sender=null;
+      var inputData="dilshat hewzulla";
+      class TestGlobalInputReceiver extends GlobalInputReceiver {
+          render(){
+            return null;
+          }
+          getGlobalInputConfig(){
+            return {
+              url:"http://192.168.0.5:1337",
+              metadata:[
+                {
+                  name:"Content",
+                  value:"dilshat",
+                  onInput:function(message){
+                      expect(message).toBe(inputData);
+                      receiver.componentWillUnmount();
+                      sender.componentWillUnmount();
+                      done();
+                  }
+                },
+                 {
+                   name:"Submit",
+                   type:"action",
+                   onInput:function(message){
+                     console.log("**** Submit message received"+JSON.stringify(message));
+                   }
+                 }
+              ]
+            }
+          }
 
 
+      }
+
+      class TestGlobalInputSender extends GlobalInputSender {
+        render(){
+          return (<div/>);
+        }
+        onInputPermissionResult(message){
+          super.onInputPermissionResult(message);
+          console.log("********sender is ready");
+          this.setGlobalInputData(0, inputData);
+        }
+      }
+
+      var onReceiverReady=function(done, registeredMessage,options){
+        done();
+        console.log("**********receiver is ready******");
+        var codedata=receiver.connector.buildInputCodeData();
+
+        const senderRenderer = renderer.create(
+            <TestGlobalInputSender codedata={codedata} ref={(input) =>{sender=input}}/>
+          );
+
+
+      }
+      const receiverRenderer = renderer.create(
+          <TestGlobalInputReceiver onRegistered={onReceiverReady} ref={(input) =>{receiver=input}}/>
+      );
+
+
+
+
+});
+
+
+/*
 
 test("encrypt and decrypt should work", function(done){
   const testInput=new TestGlobalInputReceiver();
-  const sender=createMessageConnector();
+  const sender=new TestGlobalInputSender();
+
   var inputData={
             id:"11222",
             value:"dilshat hewzulla",
@@ -36,26 +106,33 @@ test("encrypt and decrypt should work", function(done){
          }
       ]
   };
+  class TestGlobalInputReceiver extends GlobalInputReceiver {
+      getGlobalInputConfig(){
+        return this.config;
+      }
+    render() {
+      return null;
+    }
+  }
+
+  class TestGlobalInputSender extends GlobalInputSender {
+    render() {
+      return null;
+    }
+    onInputPermissionResult: function(message){
+      super.onInputPermissionResult(message);
+      expect(message.metadata[0].name).toBe(config.metadata[0].name);
+      expect(message.metadata[0].value).toBe(config.metadata[0].value);
+      expect(message.metadata[1].name).toBe(config.metadata[1].name);
+    }
+  }
 
   testInput.config=config;
   testInput.componentWillMount();
   testInput.render();
   const codeData=testInput.connector.buildInputCodeData();
 
-  console.log("receiver client:"+testInput.connector.client);
-  console.log("receiver session:"+testInput.connector.session);
-  console.log("sender client:"+sender.client);
-  console.log("codedata:"+JSON.stringify(codeData));
-  var senderOptions={
-      onInputPermissionResult: function(message){
-        expect(message.metadata[0].name).toBe(config.metadata[0].name);
-        expect(message.metadata[0].value).toBe(config.metadata[0].value);
-        expect(message.metadata[1].name).toBe(config.metadata[1].name);
-        console.log("sender sending the input message:"+JSON.stringify(inputData));
-        sender.sendInputMessage(inputData);
-      }
-    };
-    sender.processCodeData(senderOptions,codeData);
+
 
 
 
@@ -63,14 +140,4 @@ test("encrypt and decrypt should work", function(done){
 
 
 });
-
-
-
-class TestGlobalInputReceiver extends GlobalInputReceiver {
-    getGlobalInputConfig(){
-      return this.config;
-    }
-  render() {
-    return null;
-  }
-}
+*/
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {createMessageConnector} from "global-input-message";
 import QRCode from "qrcode.react";
 
 
-export  class GlobalInputReceiver extends Component {
+export   class GlobalInputReceiver extends Component {
   getGlobalInputConfig(){
     return  {};
   }
@@ -17,6 +17,9 @@ onInput(inputMessage){
           this.connector=createMessageConnector();
           var config=this.getGlobalInputConfig();
           var options={onInput:this.onInput.bind(this)};
+          if(this.props.onRegistered){
+              options.onRegistered=this.props.onRegistered;
+          }
           if(config.metadata){
               options.metadata=config.metadata.map(function(m){
                   var metadata=Object.assign({},m);
@@ -57,4 +60,76 @@ componentWillUnmount(){
   render() {
     return null;
   }
+}
+
+
+
+
+
+export  class GlobalInputSender extends Component{
+
+     constructor(props){
+        super(props);
+       this.connector=createMessageConnector();
+     }
+     createUUID() {
+       function s4() {
+         return Math.floor((1 + Math.random()) * 0x10000)
+           .toString(16)
+           .substring(1);
+       }
+       function getTimeValue(){
+         return new Date().getTime();
+       }
+       return s4()+'-'+getTimeValue();
+     }
+     disconnect(){
+       this.connector.disconnect();
+     }
+
+     componentWillUnmount(){
+          this.disconnect();
+    }
+     componentWillMount(){
+        const codedata=this.props.codedata;
+        this.processCodeData(codedata);
+      }
+     processCodeData(codedata){
+             var that=this;
+             var options={
+                onInputPermissionResult:this.onInputPermissionResult.bind(this)
+             };
+             this.connector.processCodeData(options,codedata);
+   }
+   onInputPermissionResult(message){
+     console.log("*******sender recenved the onInputPermissionResult****");
+     var globalInputdata=message.metadata;
+     globalInputdata.forEach(dataitem=>{
+       if(!dataitem.value){
+         dataitem.value="";
+       }
+     });
+     this.setState(Object.assign({}, this.state,{globalInputdata}));
+
+   }
+   setGlobalInputData(index, value){
+       if(!this.state.globalInputdata){
+            console.log("ignored:"+index+":"+value);
+            return;
+       }
+        var globalInputdata=this.state.globalInputdata.slice(0);
+         console.log("setting index:"+index+"value:"+value);
+         globalInputdata[index].value=value;
+         var message={
+             id:this.createUUID,
+             value,
+             index
+           };
+      this.connector.sendInputMessage(message);
+      this.setState(Object.assign({}, this.state,{globalInputdata}));
+  }
+  render() {
+    return null;
+  }
+
 }
