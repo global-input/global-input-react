d83fd6f5ef47e498e57af6ce1b4db9bfc0cbdd18
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-22 17:27:52 +0100

    update

--- src/GlobalInputReceiver.js
+++ src/GlobalInputReceiver.js
@@ -43,25 +43,28 @@ onInput(inputMessage){
 }
 displayInputCode(){
       const codedata=this.connector.buildInputCodeData();
-      console.log("**codedata to be displayed:[["+codedata+"]]");
+      console.log("*****input code[["+codedata+"]]");
       return(
          <QRCode value={codedata}/>
       );
 }
 displayApiCode(){
       const codeData=this.connector.buildInputCodeData();
+      console.log("*****api code[["+codeData+"]]");
       return(
          <QRCode value={codeData}/>
       );
 }
 displaySessionGroupCode(){
       const codeData=this.connector.buildSessionGroupCodeData();
+      console.log("*****sessionGroup code[["+codeData+"]]");
       return(
          <QRCode value={codeData}/>
       );
 }
 displayAESCodeData(){
       const codeData=this.connector.buildCodeAESCodeData();
+      console.log("*****CodeAES[["+codeData+"]]");
       return(
          <QRCode value={codeData}/>
       );
