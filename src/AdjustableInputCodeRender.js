d741ec43e296010939e7ebad86e1c83623d584f9
Dilshat Hewzulla <hewzulla@gmail.com>
2017-08-05 20:13:02 +0100

    update

--- src/AdjustableInputCodeRender.js
+++ src/AdjustableInputCodeRender.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-import  "./InputCodeRender.css";
+
 import AdjustableCodeDataRenderer from "./AdjustableCodeDataRenderer";
 import SendersConnected from "./SendersConnected";
 
