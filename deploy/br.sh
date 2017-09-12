aed4a30328cba9772030646df7752fb8e1877a51
Dilshat Hewzulla <hewzulla@gmail.com>
2017-09-12 23:04:36 +0100

    update

---
+++ deploy/br.sh
@@ -0,0 +1,3 @@
+browserify  -r ./dist/index.js:global-input-react > dist/global-input-react.js
+ssh root@globalinput.co.uk 'mkdir global-input-node/nginx/data/websites/globalinput/lib'
+scp dist/global-input-react.js root@globalinput.co.uk:global-input-node/nginx/data/websites/globalinput/lib
