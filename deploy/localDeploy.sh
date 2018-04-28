1be7a08e6a27f8decd17fa01c76050e910e67cca
Dilshat Hewzulla <hewzulla@gmail.com>
2018-04-28 12:08:18 +0100

    upgrading hoek dependencies

--- deploy/localDeploy.sh
+++ deploy/localDeploy.sh
@@ -1,4 +1,6 @@
 #mkdir ../global-input-web/src/global-input-react
 #cp src/*.* ../global-input-web/src/global-input-react
-mkdir ~/box/image-client-app/src/global-input-react
-rsync -azvv src/ ~/box/image-client-app/src/global-input-react/
+#mkdir ~/box/image-client-app/src/global-input-react
+#rsync -azvv src/ ~/box/image-client-app/src/global-input-react/
+mkdir ../global-input-web/src/global-input-react
+rsync -azvv src/ ../global-input-web/src/global-input-react/
