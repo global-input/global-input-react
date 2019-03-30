cc405d5ef534351ee3f49a31ac4683c9e946f962
Dilshat Hewzulla <hewzulla@gmail.com>
2019-03-30 12:06:04 +0000

    changed to the proper react component

--- src/__tests__/sender-receiver-comm.test.js
+++ src/__tests__/sender-receiver-comm.test.js
@@ -1,24 +1,13 @@
 import {CodeDataRenderer} from "../index";
-import React, {Component} from 'react';
+import React from 'react';
+import GlobalInputConnect from '../index';
 
 import renderer from 'react-test-renderer';
-import {createMessageConnector} from "global-input-message";
-
 
 test("sender and receiver communication", function(done){
-  var receiver=null;
-  var sender=null;
-  var service={
-
-  }
-  var config={};
-  var component=renderer.create(
-  <CodeDataRenderer  service={service} config={config} showControl={true}/>
-  );
-service.componentWillUnmount();
-
-
-done();
-
-
+        var globalinputComponent=renderer.create(
+              <GlobalInputConnect/>
+        );
+        //console.log(globalinputComponent.toJSON());
+      done();
 });
