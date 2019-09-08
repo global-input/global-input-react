2f476e3823602404c4b4153ce295d401fb388825
Dilshat Hewzulla <hewzulla@gmail.com>
2019-09-08 19:24:50 +0100

    upgraded dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,6 @@
-const presets = [
-    [
-    '@babel/preset-env',
-      {
-        targets: {
-          edge: "17",
-          firefox: "60",
-          chrome: "67",
-          safari: "11.1",
-        }        
-      },
-    ],
+  const presets = [
+      ['@babel/preset-env'],
+      ["@babel/preset-react"]
   ];
-  
-  module.exports = { presets:[
-       ["@babel/preset-env"
-      ]
-
-
-  ] };
+  plugins=["@babel/plugin-proposal-class-properties"]
+  module.exports = { presets,plugins};
