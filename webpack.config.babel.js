d84126c3e8b5366772b5bfbd4c90ed5ab390c2f7
Dilshat Hewzulla <hewzulla@gmail.com>
2019-09-08 15:22:12 +0100

    upgraded dev dependencies

---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+const path = require('path')
+var webpackconfig={
+    entry: './src/index.js',
+    mode:'development',
+    output: {      
+      filename: 'globalinputmessage-react.js',
+      path: __dirname + '/dist'
+      
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            exclude: /(node_modules|bower_components|build)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/env']
+            }
+        }
+        }]
+    }
+  };
+module.exports = webpackconfig;
+
+  console.log("----**----:"+webpackconfig.output.path+":****"+__dirname);
