22ed3b894e4e93a3d2d928a8fb2e0086800ab236
Dilshat Hewzulla <hewzulla@gmail.com>
2020-11-23 20:54:58 +0000

    removed css

--- src/globalinput.js
+++ src/globalinput.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import QRCode from "qrcode.react";
 import { createMessageConnector } from 'global-input-message';
-import './style.css';
 
 const ACTION_TYPES = {
     START_CONNECT: 1,
@@ -337,7 +336,12 @@ const styles = {
     }
 };
 
-export const loading = (<div className="globalInputQRCodeLoading">...</div>);
+export const loading = (<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50">
+    <path fill="#C779D0" d="M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z">
+        <animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.8s" repeatCount="indefinite" />
+    </path>
+</svg>);
+
 
 export const qrCodeLabel = (
     <div style={styles.label}>
